8ecd7557dfd92d38f69c4f4a9ade670643ec1f45
Jonny Moreira <jonnymoreira03@hotmail.com>
2024-05-12 15:46:48 -0400

    barely any changes -_-

--- ui_test.py
+++ ui_test.py
@@ -1,19 +1,36 @@
 import sys
-from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5 import uic
 from PyQt5 import QtGui
+from pymongo import MongoClient
+
+client = MongoClient("mongodb+srv://foodlover:CDOG2CI3GApYWkJv@foodlover.xagchl4.mongodb.net/")
+db = client['users']
 
 class MainWindow(QMainWindow):
 
     def __init__(self):
         super(MainWindow, self).__init__()
-        uic.loadUi("store_main.ui", self)
-        self.manager_btn.clicked.connect(self.test)
+        uic.loadUi("manager.ui", self)
 
-    def test(self):
-        text, ok = QInputDialog.getText(self, "ID", "Enter your ID:")
-        if ok:
-            QMessageBox.information(self, "Information", f'Hello, {text}!')
+        workers = db['workers']
+        usr_manager = workers.find_one({'position': 'Manager'})
+        self.welcome_manager_txt.setText("Welcome, " + usr_manager['name']) # type: ignore
+
+        self.wages_btn.clicked.connect(self.wages)
+
+    def wages(self):
+        self.wage_win = Wages()
+        self.wage_win.show()
+
+
+class Wages(QMainWindow):
+    def __init__(self):
+        super(Wages, self).__init__()
+        uic.loadUi("wages.ui", self)
+        self.setWindowTitle("Wages")
+
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
